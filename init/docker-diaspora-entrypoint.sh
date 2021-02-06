58629a3a37837563615247ed82ea625c1952b8e2
kipjr <martijn@koetsier.dev>
2021-02-06 17:26:34 +0000

    Almost working. Stuck on adapter

--- init/docker-diaspora-entrypoint.sh
+++ init/docker-diaspora-entrypoint.sh
@@ -1,27 +1,26 @@
 #!/bin/bash
 
 function do_as_diaspora {
-	cd /home/diaspora/diaspora && RAILS_ENV=production DB=MariaDB $1
-}
-
-function setup {
-	. /run_as_diaspora.sh
+	 "cd /home/diaspora/diaspora && RAILS_ENV=production DB=postgres $1"
 }
 
 function run {
-	echo "Staring Diaspora"
-	do_as_diaspora "./script/server"
-	echo "Really wasn't expecting this"
-}
+	echo "Starting Diaspora"
+	cd /home/diaspora/diaspora
+        RAILS_ENV=production DB=postgres bin/bundle install
+	RAILS_ENV=production ./bin/rake assets:precompile
+	#git checkout Gemfile.lock db/schema.rb && git pull && cd .. && cd - && 
+        #bin/bundle && bin/rake db:migrate 
+        #./script/server
 
-function bundle {
-	do_as_diaspora "gem install bundler && bin/bundle"
+	echo "Really wasn't expecting this"
+
 }
 
 function init_db {
-	do_as_diaspora "bin/rake db:create db:schema:load" && \
-	  precompile_assets && \
-	  run
+	do_as_diaspora "bin/rake db:create db:schema:load"
+	precompile_assets
+	run
 }
 
 function precompile_assets {
@@ -30,19 +29,18 @@ function precompile_assets {
 
 echo "Starting docker-entrypoint with argument '$1'"
 
+
+
+
 if [ "$1" = 'run' ]; then
 	run
-elif [ "$1" = 'bundle' ]; then
-	bundle
-elif [ "$1" = 'setup' ]; then
-	setup
 elif [ "$1" = 'init-db' ]; then
 	init_db
 elif [ "$1" = 'upgrade' ]; then
 	do_as_diaspora "rvm update"
 	do_as_diaspora "git checkout Gemfile.lock db/schema.rb && git pull && cd .. && cd - && gem install bundler && bin/bundle && bin/rake db:migrate "
 	precompile_assets
-else
+else 
 	echo "Not sure what to do; here have a shell"
-	/bin/bash &
+	/bin/bash
 fi
--- init/run_as_diaspora.sh
+++ init/run_as_diaspora.sh
@@ -1,10 +1,9 @@
 #!/bin/bash --login
 
-
 cd $HOME && \
   curl -L https://s.diaspora.software/1t | bash && \
   echo '[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"' >> $HOME/.bashrc && \
   source "/home/diaspora/.rvm/scripts/rvm" && \
   rvm autolibs read-fail && \
-  rvm install 2.6 && \
+  rvm install 2.2 && \
   git clone https://github.com/diaspora/diaspora.git
